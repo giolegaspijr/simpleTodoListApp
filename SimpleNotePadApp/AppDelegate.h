5dfdd1195f5f4b3f8b22e1a75727708926b8b934
Sergio Legaspi Jr <sergiolegaspijr@Sergios-MacBook-Pro-2.local>
2017-02-27 18:15:57 +0800

    Initial Commit

---
+++ SimpleNotePadApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleNotePadApp
+//
+//  Created by Sergio Legaspi Jr. on 27/02/2017.
+//  Copyright © 2017 Sergio Legaspi Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleNotePadApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleNotePadApp
+//
+//  Created by Sergio Legaspi Jr. on 27/02/2017.
+//  Copyright © 2017 Sergio Legaspi Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
